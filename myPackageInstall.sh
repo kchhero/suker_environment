206c51c8c46e9a65bb43d8a56775059ca66ebe15
choonghyun-Jeon <suker@nexell.co.kr>
2016-05-12 16:15:00 +0900

    some files update

--- myPackageInstall.sh
+++ myPackageInstall.sh
@@ -1,4 +1,7 @@
+sudo apt-get install terminator
+
 sudo add-apt-repository ppa:umang/indicator-stickynotes
 sudo apt-get update
 sudo apt-get install indicator-stickynotes
 
+sudo apt-get install git dkms build-essential vim libtool autoconf unrar libncurses5-dev python subversion tftpd-hpa tftp-hpa nfs-kernel-server lynx curl minicom ia32-libs device-tree-compiler
