8c0d5a1279ad0828c4e9ae7a1f989df930bee145
choonghyun-Jeon <suker@nexell.co.kr>
2016-07-01 17:33:17 +0900

    myPackage script update

--- myPackageInstall.sh
+++ myPackageInstall.sh
@@ -1,10 +1,19 @@
-sudo apt-get install terminator
+#default my env
+sudo apt-get install emacs24 vim
 
+#terminal utility
+sudo apt-get install terminator
+#hex-editor
+sudo apt-get install bless
+#serial terminal
+sudo apt-get install ckermit
+
+#Nexell develop Env.
 sudo apt-get install git dkms build-essential libtool autoconf unrar libncurses5-dev python subversion tftpd-hpa tftp-hpa nfs-kernel-server 
 sudo apt-get install lynx curl device-tree-compiler
 
 sudo apt-get install libpciaccess-dev autoconf autogen xutils-dev libtool libpthread-stubs0-dev repo
-sudo apt-get install emacs24 vim putty minicom fastboot
+sudo apt-get install putty minicom fastboot
 
 #Yocto
 sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib chrpath socat libsdl1.2-dev xterm lzop
@@ -14,6 +23,11 @@ sudo add-apt-repository ppa:umang/indicator-stickynotes
 sudo apt-get update && sudo apt-get install indicator-stickynotes
 sudo apt-get install ia32-libs
 
+#desktop
+sudo apt install unity-control-center unity-tweak-tool
+
+#python
+sudo apt-get install wxglade spyder
 sudo apt install python-pip
 pip install --upgrade pip
 #pip install django==1.6
